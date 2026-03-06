70554b28800d7af29b361a3a02c6fc9d0e1ccd47
TakeiCorp <herve.rome@gmail.com>
2026-03-06 13:25:20 +0100

    Add files via upload

---
+++ drivers.js
@@ -0,0 +1,31 @@
+
+/*
+MODIFIER LES PILOTES ICI SI BESOIN
+
+Si la grille F1 change, modifie simplement cette liste.
+*/
+
+const drivers = [
+"Lando Norris",
+"Oscar Piastri",
+"Max Verstappen",
+"Isack Hadjar",
+"Charles Leclerc",
+"Lewis Hamilton",
+"George Russell",
+"Kimi Antonelli",
+"Fernando Alonso",
+"Lance Stroll",
+"Pierre Gasly",
+"Franco Colapinto",
+"Alexander Albon",
+"Carlos Sainz",
+"Esteban Ocon",
+"Oliver Bearman",
+"Liam Lawson",
+"Arvid Lindblad",
+"Nico Hulkenberg",
+"Gabriel Bortoleto",
+"Sergio Perez",
+"Valtteri Bottas"
+]
