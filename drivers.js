5f3a7948fcf8e65b9d1b3307c702e24c3c79dac3
TakeiCorp <herve.rome@gmail.com>
2026-03-06 15:05:43 +0100

    Update drivers.js

--- drivers.js
+++ drivers.js
@@ -1,31 +1,36 @@
+window.drivers = [
 
-/*
-MODIFIER LES PILOTES ICI SI BESOIN
-
-Si la grille F1 change, modifie simplement cette liste.
-*/
-
-const drivers = [
-"Lando Norris",
-"Oscar Piastri",
 "Max Verstappen",
 "Isack Hadjar",
+
 "Charles Leclerc",
 "Lewis Hamilton",
+
 "George Russell",
 "Kimi Antonelli",
+
+"Lando Norris",
+"Oscar Piastri",
+
 "Fernando Alonso",
 "Lance Stroll",
+
 "Pierre Gasly",
 "Franco Colapinto",
+
 "Alexander Albon",
 "Carlos Sainz",
+
 "Esteban Ocon",
 "Oliver Bearman",
+
 "Liam Lawson",
 "Arvid Lindblad",
+
 "Nico Hulkenberg",
 "Gabriel Bortoleto",
+
 "Sergio Perez",
 "Valtteri Bottas"
+
 ]
