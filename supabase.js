f43cd23b282245183b81e5291126cf50249c610a
TakeiCorp <herve.rome@gmail.com>
2026-03-06 13:46:50 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,2 +1,3 @@
 const SUPABASE_URL = "https://abcxyz.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
